69871c9951e5db164812fae8efa9c503ee87abca
Jessica <jckjessica@hotmail.fr>
2022-11-06 20:19:57 +0100

    Snd commit

---
+++ ponyracer/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="container" style="margin-top: 70px">
+    <h1>Ponyracer</h1>
+    <h2>Welcome {{ user.name }}</h2>
+    <Races></Races>
+    <!-- <h2>{{ numberOfUsers }} users</h2> -->
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Races from './Races.vue';
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    Races
+  },
+  setup() {
+    /* const numberOfUsers = ref(146);
+    setTimeout(() => (numberOfUsers.value = 147), 3000);
+    return { numberOfUsers }; */
+    const user = ref({
+      id: 1,
+      name: 'Jessica'
+    });
+    return { user }; 
+  }
+});
+</script>
+
+<style></style>
---
+++ ponyracer/src/Races.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Races</h2>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Races'
+});
+</script>
